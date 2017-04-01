bdf8529a20226fa1d651a77ecc32f21cb9276dc7
dortley <dortley@humbolt>
2017-03-31 22:52:51 -0600

    More work done on grid acceleration code.  Not complete yet.
    Committing non-working code.  Currently working on pseudo-constructor (generator function, which
    should be a constructor.)

--- src/GridAccel.jl
+++ src/GridAccel.jl
@@ -3,11 +3,19 @@ module GridAccel
 export GridAccel, GenerateStructure
 
 using ArrayEnv
+# a=Array(Array{Int32,1}, 2, 2)
+# a[1,1] = Array(Int32, 0)
+# push!(a[1,1],<an integer>)
+
 
 type AccelGrid{T}
     count::T
-    a::Array{T}
+    a::Array{Array{T,1},3}
     oa::ObjectArray
+
+    xmin::Float64, xmax::Float64
+    ymin::Float64, ymax::Float64
+    zmin::Float64, zmax::Float64
 end
 
 Base.start(::AccelGrid) = 1
@@ -29,17 +37,33 @@ function Base.setindex!(AA::AccelGrid, v::Integer, i::Integer)
 end
 
 
-function GenerateStructure(OA::ObjectArray, nx, ny, nz)::AccelGrid
-    local n = length(OA)
+function GenerateStructure(OA::ObjectArray, nx, ny, nz)
+    local count = length(OA)
     local xmin::Float64, xmax::Float64
     local ymin::Float64, ymax::Float64
     local zmin::Float64, zmax::Float64
+    local x::Float64, y::Float64, z::Float64
 
-    # x::Float64[]
+    xmin,ymin,zmin = OA[1].center - OA[1].radius
+    xmax,ymax,zmax = OA[1].center + OA[1].radius
+    for o in OA
+        xmin,ymin,zmin = min(o.center - o.radius, [xmin, ymin, zmin])
+        xmax,ymax,zmax = max(o.center + o.radius, [xmax, ymax, zmax])
+    end
     
-    a = Array(typeof(n), n)
-    for i in 1:n
-        a[i] = i
+    a = Array(Array{Int32,1}, nx, ny, nz)
+    for i in 1:nx
+        for j in 1:ny
+            for k in 1:nz
+                a[i,j,k] = Array(Int32, 0)
+            end
+        end
+    end
+
+    for o in 1:count
+        xmn,ymn,zmn = o.center - o.radius
+        xmx,ymx,zmx = o.center + o.radius
+        # TODO: Add selected sphere to accel structure
     end
     AccelGrid(n, a, OA)
 end

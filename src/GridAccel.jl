81c5a7646650349881715c23b145f4ad8b392a79
dortley <dortley@humbolt>
2017-09-04 22:54:53 -0600

    Grid Accel structure is in place and working.
    Using a grid-based acceleration structure for handling large (+millions) of
    particles.
    Lots of optimizations left to do
    Lots of old code still floating around that I'm keeping around as I develop.

--- src/GridAccel.jl
+++ src/GridAccel.jl
@@ -1,10 +1,11 @@
 module GridAccel
 
-export GridAccel, GridAccel_Iter, GenerateStructure
+export AccelGrid, GridAccel_CellIter, GenerateStructure
+export box_exit
 
 using ArrayEnv
-# a=Array(Array{Int32,1}, 2, 2)
-# a[1,1] = Array(Int32, 0)
+# a=Array{Array{Int32,1}}, 2, 2)
+# a[1,1] = Array{Int32},0)
 # push!(a[1,1],<an integer>)
 
 
@@ -23,25 +24,48 @@ type AccelGrid{T}
     dy::Float64
     dz::Float64
 
-    nx::Int64
-    ny::Int64
-    nz::Int64
+    nx::T
+    ny::T
+    nz::T
 end
 
-type AccelGrid_Iter{T}
+type AccelGrid_CellIter{T}
     acc_grd::AccelGrid
     origin_ray::Array{T,3}
-    cur_position::Array{Int32,3}
+    cur_position::Array{T,3}
+    cur_itter::T
+    cur_max::T
 end
 
-Base.start(::AccelGrid_Iter) = 1
-Base.next(AA::AccelGrid_Iter, state) = (AA.oa[AA.a[state]], state+1)
-Base.done(AA::AccelGrid_Iter, state) = state > AA.count
+function Base.start(::AccelGrid_CellIter)
+end
 
-Base.eltype(::Type{AccelGrid_Iter}) = AccelGrid_Iter.acc_grd
-Base.length(AA::AccelGrid_Iter) = AA.count
-Base.size(AA::AccelGrid_Iter) = (AA.count,)
-Base.endof(AA::AccelGrid_Iter) = AA.count
+function Base.next(AG::AccelGrid_CellIter, state)
+    cur_cell = AG.acc_grd.a[AG.cur_position[1], AG.cur_position[2], AG.cur_position[3]]
+    if AG.cur_itter < cur_cell.length()
+        AG.cur_itter = AG.cur_itter+1
+    else
+        
+    end
+end
+
+function Base.done(AG::AccelGrid_CellIter, state)
+    cur_cell = AG.acc_grd.a[AG.cur_position[1], AG.cur_position[2], AG.cur_position[3]]
+    if AG.cur_itter > cur_cell.length()
+        # check if exiting the grid
+    end
+    return false
+end
+
+#function Base.eltype(::Type{AccelGrid_CellIter}) = AccelGrid_CellIter.acc_grd
+#function Base.length(AG::AccelGrid_CellIter) = AG.count
+#function Base.size(AG::AccelGrid_CellIter)
+#    return (AG.acc_grd.nx,
+#            AG.acc_grd.ny,
+#            AG.acc_grd.nz)
+#end
+
+#function Base.endof(AG::AccelGrid_CellIter) = AG.count
 
 function Base.getindex(AA::AccelGrid, i::Integer)
     1 <= i <= AA.count || throw(BoundsError(AA,i))
@@ -51,6 +75,79 @@ end
 function Base.setindex!(AA::AccelGrid, v::Integer, i::Integer)
     1 <= i <= AA.count || throw(BoundsError(AA,i))
     AA.a[i] = v
+end
+
+#bool intersection(box b, ray r) {
+#    double tx1 = (b.min.x - r.x0.x)*r.n_inv.x;
+#    double tx2 = (b.max.x - r.x0.x)*r.n_inv.x;
+# 
+#    double tmin = min(tx1, tx2);
+#    double tmax = max(tx1, tx2);
+# 
+#    double ty1 = (b.min.y - r.x0.y)*r.n_inv.y;
+#    double ty2 = (b.max.y - r.x0.y)*r.n_inv.y;
+# 
+#    tmin = max(tmin, min(ty1, ty2));
+#    tmax = min(tmax, max(ty1, ty2));
+# 
+#    return tmax >= tmin;
+#}
+function box_exit(x1, x2, y1, y2, z1, z2, ray_orig, ray_dir)
+    local dx::Float64
+    local dy::Float64
+    local dz::Float64
+    local sx::Int32
+    local sy::Int32
+    local sz::Int32
+
+    if ray_dir[1] < 0.0
+        dx = -(ray_orig[1] - x1) / ray_dir[1]
+        sx = -1 
+    elseif ray_dir[1] > 0.0
+        dx = (x2 - ray_orig[1]) / ray_dir[1]
+        sx = 1
+    else
+        dx = Inf
+        sx = 0
+    end
+
+    if ray_dir[2] < 0.0
+        dy = -(ray_orig[2] - y1) / ray_dir[2]
+        sy = -2
+    elseif ray_dir[2] > 0.0
+        dy = (y2 - ray_orig[2]) / ray_dir[2]
+        sy = 2
+    else
+        dy = Inf
+        sy = 0
+    end
+
+    if ray_dir[3] < 0.0
+        dz = -(ray_orig[3] - z1) / ray_dir[3]
+        sz = -3
+    elseif ray_dir[3] > 0.0
+        dz = (z2 - ray_orig[3]) / ray_dir[3]
+        sz = 3
+    else
+        dz = Inf
+        sz = 0
+    end
+
+    if dy < dx
+        if dz < dy
+            return sz
+        else
+            return sy
+        end
+    else
+        if dz < dx
+            return sz
+        else
+            return sx
+        end
+    end
+
+    return 5
 end
 
 
@@ -65,25 +162,32 @@ function GenerateStructure(OA::ObjectArray, nx, ny, nz)
     xmin,ymin,zmin = OA[1].center - OA[1].radius
     xmax,ymax,zmax = OA[1].center + OA[1].radius
     for o in OA
-        xmin,ymin,zmin = min((o.center - o.radius) - 0.1, [xmin, ymin, zmin])
-        xmax,ymax,zmax = max((o.center + o.radius) + 0.1, [xmax, ymax, zmax])
+        xmin,ymin,zmin = min.((o.center - o.radius) - 0.1, [xmin, ymin, zmin])
+        xmax,ymax,zmax = max.((o.center + o.radius) + 0.1, [xmax, ymax, zmax])
     end
 
     dx,dy,dz = ([xmax,ymax,zmax] - [xmin,ymin,zmin]) ./ [nx, ny, nz]
     
-    a = Array(Array{Int64,1}, nx, ny, nz)
+    a = Array{Array{Int64,1}}(nx, ny, nz)
     for i in 1:nx
         for j in 1:ny
             for k in 1:nz
-                a[i,j,k] = Array(Int32, 0)
+                a[i,j,k] = Array{Int32}(0)
             end
         end
     end
 
     for s in 1:count
         o = OA[s]
-        xmn,ymn,zmn = floor(Int32, (o.center - o.radius - [xmin, ymin, zmin]) ./ [dx, dy, dz]) + 1
-        xmx,ymx,zmx = floor(Int32, (o.center + o.radius - [xmin, ymin, zmin]) ./ [dx, dy, dz]) + 1
+
+        xmn,ymn,zmn = (o.center - o.radius - [xmin, ymin, zmin]) ./ [dx, dy, dz]
+        xmx,ymx,zmx = (o.center + o.radius - [xmin, ymin, zmin]) ./ [dx, dy, dz]
+        xmn = floor(Int32, xmn) + 1
+        ymn = floor(Int32, ymn) + 1
+        zmn = floor(Int32, zmn) + 1
+        xmx = floor(Int32, xmx) + 1
+        ymx = floor(Int32, ymx) + 1
+        zmx = floor(Int32, zmx) + 1
 
         for i in xmn:xmx
             for j in ymn:ymx
--- src/RayTrace.jl
+++ src/RayTrace.jl
@@ -2,7 +2,7 @@ module RayTrace
 
 export Camera, render
 
-using MatrixTools, JuliaShader, Geometries
+using MatrixTools, JuliaShader, Geometries, GridAccel
 
 # Object related code
 type Camera{T,U}
@@ -43,19 +43,19 @@ function Camera{T<:Integer}(res_x::T, res_y::T)
 end
 
 function CalcPPU{T<:Integer, U<:AbstractFloat}(res_x::T, res_y::T, focal_length::U, fov::U)
-    f_x = convert(Float64, res_x)
-    f_y = convert(Float64, res_y)
+    f_x::U = convert(U, res_x)
+    f_y::U = convert(U, res_y)
 
-    opposite_half = sqrt(f_x^2 + f_y^2)/2.0
-    opposite_size = focal_length * tan(fov/2.0)
+    opposite_half::U = sqrt(f_x^2 + f_y^2)/2.0
+    opposite_size::U = focal_length * tan(fov/2.0)
 
     return opposite_size / opposite_half
 end
 
 
 function get_ray(camera::Camera, i, j, jitter=false)
-    p_x = convert(Float64, i - camera.res_x/2) * camera.ppu
-    p_y = convert(Float64, j - camera.res_y/2) * camera.ppu
+    p_x::Float64 = convert(Float64, i - camera.res_x/2) * camera.ppu
+    p_y::Float64 = convert(Float64, j - camera.res_y/2) * camera.ppu
 
     if jitter
         p_x += 0.2*randn()*camera.ppu
@@ -78,17 +78,157 @@ function get_ray(camera::Camera, i, j, jitter=false)
 end
 
 
-function trace_path(world_objects, ray_orig, ray_dir, depth)
+function trace_path(accel_struct, ray_orig, ray_dir, depth::Int32)
+    local new_glossy_ray_dir::Array{Float64,1}
+    local new_diffuse_ray_dir::Array{Float64,1}
+
     local diffuse_color::ShaderRGBA
     local glossy_color::ShaderRGBA
     local emission_color::ShaderRGBA
+
+    local glossy_mix::Float64
+    local emission_mix::Float64
+
+    local world_objects = accel_struct.oa  # world_objects is the object array
 
     if depth == 0
         return ShaderRGBA(0.0, 0.0, 0.0)
     end
 
-    selected_item=0
-    closest_dist=1e100
+    selected_item = 0
+    closest_dist = 1e100
+
+    if (ray_orig[1] >= accel_struct.xmin) && (ray_orig[1] <= accel_struct.xmax) &&
+       (ray_orig[2] >= accel_struct.ymin) && (ray_orig[2] <= accel_struct.ymax) &&
+       (ray_orig[3] >= accel_struct.zmin) && (ray_orig[3] <= accel_struct.zmax)
+
+        grid_i = convert(Int64, ceil( (ray_orig[1] - accel_struct.xmin)/accel_struct.dx ))
+        grid_j = convert(Int64, ceil( (ray_orig[2] - accel_struct.ymin)/accel_struct.dy ))
+        grid_k = convert(Int64, ceil( (ray_orig[3] - accel_struct.zmin)/accel_struct.dz ))
+
+        while true
+            a = accel_struct.a[grid_i, grid_j, grid_k]
+            for i in 1:length(a)
+                hit,dist = obj_intersect(world_objects[a[i]], ray_orig, ray_dir)
+                if hit
+                    if dist < closest_dist
+                        selected_item=i
+                        closest_dist=dist
+                    end
+                end
+            end
+            selected_item > 0 && break
+
+            x1 = accel_struct.dx*(grid_i-1) + accel_struct.xmin
+            x2 = accel_struct.dx*grid_i + accel_struct.xmin
+            y1 = accel_struct.dy*(grid_j-1) + accel_struct.ymin
+            y2 = accel_struct.dy*grid_j + accel_struct.ymin
+            z1 = accel_struct.dz*(grid_k-1) + accel_struct.zmin
+            z2 = accel_struct.dz*grid_k + accel_struct.zmin
+
+            bx_ex = box_exit(x1, x2, y1, y2, z1, z2, ray_orig, ray_dir)
+            if bx_ex == 1   grid_i = grid_i + 1  end
+            if bx_ex == 2   grid_j = grid_j + 1  end
+            if bx_ex == 3   grid_k = grid_k + 1  end
+            if bx_ex == -1  grid_i = grid_i - 1  end
+            if bx_ex == -2  grid_j = grid_j - 1  end
+            if bx_ex == -3  grid_k = grid_k - 1  end
+
+            grid_i < 1               && break
+            grid_i > accel_struct.nx && break
+            grid_j < 1               && break
+            grid_j > accel_struct.ny && break
+            grid_k < 1               && break
+            grid_k > accel_struct.nz && break
+         end
+    else
+        if selected_item == 0
+            for i in 1:length(world_objects)
+                hit,dist = obj_intersect(world_objects[i], ray_orig, ray_dir)
+                if hit
+                     if dist < closest_dist
+                        selected_item=i
+                        closest_dist=dist
+                    end
+                end
+            end
+        end
+    end
+
+    # If no object is seen, return ambient light conditions (hard coded for now.)
+    if selected_item == 0
+      return ShaderRGBA(1.0, 1.0, 1.0)
+    end
+
+    diffuse_color = ShaderRGBA(0.0, 0.0, 0.0)
+    glossy_color = ShaderRGBA(0.0, 0.0, 0.0)
+    emission_color = ShaderRGBA(0.0, 0.0, 0.0)
+    ret_color = ShaderRGBA(0.0, 0.0, 0.0)
+
+    # Shader Work
+    point_hit, normal_of_hit = calc_intersection(world_objects[selected_item], ray_orig, ray_dir, closest_dist)
+
+    diffuse_set   = world_objects[selected_item].material.diffuse_set
+    glossy_set    = world_objects[selected_item].material.glossy_set
+    emission_set  = world_objects[selected_item].material.emission_set
+
+    if diffuse_set
+        new_diffuse_ray_dir = calc_diff(ray_dir, normal_of_hit)
+        ret_diffuse_color = trace_path(accel_struct, point_hit, new_diffuse_ray_dir, depth-1)
+        diffuse_color = world_objects[selected_item].material.diffuse * max(0.0, dot(normal_of_hit, new_diffuse_ray_dir))
+        diffuse_color = diffuse_color * ret_diffuse_color
+        if !glossy_set && !emission_set
+            return diffuse_color
+        end
+    else
+        glossy_mix = 1.0
+    end
+
+    if glossy_set
+        new_glossy_ray_dir = calc_refl(ray_dir, normal_of_hit)
+        ret_glossy_color = trace_path(accel_struct, point_hit, new_glossy_ray_dir, depth-1)
+        glossy_color = world_objects[selected_item].material.glossy * ret_glossy_color
+        if !diffuse_set && !emission_set
+            return glossy_color
+        end
+    else
+        glossy_mix = 0.0
+    end
+
+    if emission_set
+        emission_color = world_objects[selected_item].material.emission
+        if !diffuse_set && !glossy_set
+            return emission_color
+        end
+    else
+        emission_mix = 0.0
+    end
+
+    glossy_mix   = world_objects[selected_item].material.glossy_mix
+    emission_mix = world_objects[selected_item].material.emission_mix
+
+    return mix(emission_mix, mix(glossy_mix, diffuse_color, glossy_color), emission_color)
+end
+
+
+function trace_path2(world_objects, ray_orig, ray_dir, depth::Int32)
+    local new_glossy_ray_dir::Array{Float64,1}
+    local new_diffuse_ray_dir::Array{Float64,1}
+
+    local diffuse_color::ShaderRGBA
+    local glossy_color::ShaderRGBA
+    local emission_color::ShaderRGBA
+
+    local glossy_mix::Float64
+    local emission_mix::Float64
+
+    if depth == 0
+        return ShaderRGBA(0.0, 0.0, 0.0)
+    end
+
+    selected_item = 0
+    closest_dist = 1e100
+
     for i in 1:length(world_objects)
         hit,dist=obj_intersect(world_objects[i], ray_orig, ray_dir)
         if hit
@@ -155,15 +295,15 @@ function trace_path(world_objects, ray_orig, ray_dir, depth)
 end
 
 function render(camera::Camera, aa, samples)
-    fsamples = convert(Float64, samples)
-    img = SharedArray(Float32, 3, camera.res_y, camera.res_x)
+    fsamples::Float64 = convert(Float64, samples)
+    img = SharedArray{Float32}(3, camera.res_y, camera.res_x)
     @sync @parallel for i in 1:camera.res_x
-        println("Column: $(i)")
         for j in 1:camera.res_y
-            r = 0.0
-            g = 0.0
-            b = 0.0
-            a = 0.0
+        println("Column: $(i)  Row: $(j)")
+            r::Float64 = 0.0
+            g::Float64 = 0.0
+            b::Float64 = 0.0
+            a::Float64 = 0.0
             for itter = 1:samples
                 ray_dir = get_ray(camera, i, j)
                 x = trace_path(aa, camera.origin, ray_dir, 2)
